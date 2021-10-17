b32a783bed44cb38075a807b9ab975b46d4dcee7
Flavio Wu <flaviowu@gmail.com>
2021-10-17 12:26:37 -0300

    Initial Commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>Desenvolvido por Flavio Wu para a BlueEdTech 2021</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <ul className="navbar">
+        <li>Home</li>
+        <li>Games</li>
+        <li>User</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
