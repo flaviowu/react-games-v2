7be9d50046d181505d9c908499600a00c85d8a00
Flavio Wu <flaviowu@gmail.com>
2021-10-17 21:50:32 -0300

    implementção inicial de context hook

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Header.css";
+import { Link } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
+import { useHistory } from "react-router-dom";
 
 function Header() {
+  const { user, setUser } = useContext(UserContext);
+  const history = useHistory();
+
+  function handleLogout() {
+    setUser("");
+    history.push("/");
+  }
+
   return (
     <div className="header">
-      <ul className="navbar">
-        <li>Home</li>
-        <li>Games</li>
-        <li>User</li>
+      {user}
+      <ul className="nav-list">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/Games">Games</Link>
+        </li>
+
+        {user ? (
+          <li onClick={handleLogout}>Logout</li>
+        ) : (
+          <li>
+            <Link to="/Login">Login</Link>
+          </li>
+        )}
       </ul>
     </div>
   );
